cf7b9ff48c83f3adf00a088d5d359cad49187c50
Camille-kyo30 <gnandekouakou710@gmail.com>
2026-03-24 20:29:53 +0000

    Update out.js

--- scripts/cmds/out.js
+++ scripts/cmds/out.js
@@ -1,30 +1,53 @@
 const axios = require("axios");
 const fs = require("fs-extra");
 const request = require("request");
-module.exports = {
-	config: {
-		name: "Out",
-		aliases: ["l"],
-		version: "1.0",
-		author: "Chitron Bhattacharjee",
-		countDown: 5,
-		role: 2,
-		shortDescription: "bot will leave gc",
-		longDescription: "",
-		category: "admin",
-		guide: {
-			vi: "{pn} [tid,blank]",
-			en: "{pn} [tid,blank]"
-		}
-	},
 
-	onStart: async function ({ api,event,args, message }) {
- var id;
- if (!args.join(" ")) {
- id = event.threadID;
- } else {
- id = parseInt(args.join(" "));
+module.exports = {
+ config: {
+  name: "out",
+  aliases: ["l"],
+  version: "3.0",
+  author: "Camille 🩵",
+  countDown: 5,
+  role: 2,
+  category: "blue lock"
+ },
+
+ onStart: async function ({ api, event, args }) {
+
+  const id = !args.join(" ") ? event.threadID : parseInt(args.join(" "));
+
+  const block = "█";
+
+  const message = `
+${block}${block}${block}${block}${block}${block}${block}${block}${block}${block}
+
+⚽🔥 [FIN DU MATCH] 🔥⚽
+
+👁️ Le bot quitte le terrain…
+
+${block}${block}${block}
+
+💀 Vous êtes abandonnés.
+💬 Essayez de survivre sans moi… (bonne chance 😏)
+
+${block}${block}${block}${block}${block}
+
+🤣 Sérieusement… sans moi, ça va devenir un serveur de débutants.
+
+${block}${block}${block}${block}${block}${block}
+
+⚡ "Même Ego s’ennuie ici."
+
+${block}${block}${block}${block}${block}${block}${block}${block}
+
+👋 À plus… si vous survivez jusque-là.
+
+${block}${block}${block}${block}${block}${block}${block}${block}${block}${block}
+`;
+
+  api.sendMessage(message, id, () => {
+   api.removeUserFromGroup(api.getCurrentUserID(), id);
+  });
  }
- return api.sendMessage('𝐎𝐊 𝐁𝐘𝐄 𝐋𝐄𝐅𝐓 𝐆𝐑𝐎𝐔𝐏 🦆', id, () => api.removeUserFromGroup(api.getCurrentUserID(), id))
-		}
-	};
+};
